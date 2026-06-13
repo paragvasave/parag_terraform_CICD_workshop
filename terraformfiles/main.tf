a55383d583a146d660dcca35e302e7b7d0ec81ba
Parag Vasave <vasave.parag@gmail.com>
2026-06-13 10:26:17 +0000

    terraform files added

---
+++ terraformfiles/main.tf
@@ -0,0 +1,19 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "parag-rg-cicd-ntms-workshop"
+  location = "East US"
+}
+
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "parag-vnet-cicd-workshop"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+
+resource "azurerm_subnet" "subnet" {
+  name = "web-subnet"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name = azurerm_resource_group.rg.name
+}
