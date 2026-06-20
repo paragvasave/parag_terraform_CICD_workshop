d7e339135e5fe6cf314eef8bbc29d3caf8362b0c
parag.vasave <vasave.parag@gmail.com>
2026-06-20 11:45:48 +0000

    app-subnet2

--- terraformfiles/provider.tf
+++ terraformfiles/provider.tf
@@ -8,10 +8,10 @@ terraform {
 
 
   backend "azurerm" {
-    resource_group_name = "rg-terraform-state"
+    resource_group_name  = "rg-terraform-state"
     storage_account_name = "stntmstfstate"
-    container_name = "parag" #replace with your name
-    key = "parag.terraform.tfstate" #replace with your name
+    container_name       = "parag"                   #replace with your name
+    key                  = "parag.terraform.tfstate" #replace with your name
   }
 }
 
